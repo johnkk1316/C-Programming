b4b8de64e3e6f7e05e2e0ef34b904859c5991725
Your Name <Heracleioncapital@gmail.com>
2024-08-10 18:15:15 +0300

    Updated sntax

--- 1. C Tutorials/2. C Sntax.c
+++ 1. C Tutorials/2. C Sntax.c
@@ -10,15 +10,17 @@ int main(void)
 ----------------------------------------------------------------
 Notes Code breakdown.
 ----------------------------------------------------------------
-*Line 1: #include <stdio.h> 
-        Line 1 is a Header File Library that allows us to work with Input and Output functions, such as printf()
+*Line 1: #include <stdio.h>
+        Line 1 is a Header File Library that allows us to work with Input and Output functions, such as printf().
         Header files add functionality to C programs.
 
-*
+*Line 2: A blank line. C ignores whitespace. But we use it to make the code more readable.
 
-*
+*Line 3: main()
+        Another thing that appears in C programs is Main(). This is called a function. Any code inside its curl bracket () will be executed.
 
-*
+*Line 5: printf()
+        Printf() is a function that is used to output/print to the screen. In or example it will otput "Hello World"
 
 *
 
