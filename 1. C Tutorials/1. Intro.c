d52bbc0c683b4e0841ceb63beb6d2254cebeab4a
Your Name <Heracleioncapital@gmail.com>
2024-08-10 17:51:25 +0300

    C intro and Syntax

---
+++ 1. C Tutorials/1. Intro.c
@@ -0,0 +1,40 @@
+/*
+NOTES:
+----------------------------------------------------------------
+What is C?
+----------------------------------------------------------------
+
+* C is a general purpose programming language created by Dennis Ritchie at Bell Labs in 1972.
+
+* C is a very popular language, despite being old, the reason for its poplarity is because its a fundamental langage in the field of computer science.
+
+* Its strongly associated with UNIX, as it was developed to to write the unix opperating system
+
+----------------------------------------------------------------
+Why learn C?
+----------------------------------------------------------------
+* If you Know C, you will have no problem learning other languages such as java, python, C++, C#, etc, as the syntax is the same.
+
+* C is very fast, compared to other languages, like Java and Python.
+
+* C is Versatile; It can be used in both applications and technologies
+
+----------------------------------------------------------------
+Differences between C and C++
+----------------------------------------------------------------
+* C++ was developed as an extension of C and both languages have almost similar syntax
+
+* The difference between C and C++ is that C++ supports classes and objects, while C does not.
+
+*/
+
+// My first C Program
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world!\n");
+
+    return 0;
+}
---
+++ 1. C Tutorials/2. C Sntax.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!");
+    return 0;
+}
+
+/*
+----------------------------------------------------------------
+Notes Code breakdown.
+----------------------------------------------------------------
+*Line 1: #include <stdio.h> 
+        Line 1 is a Header File Library that allows us to work with Input and Output functions, such as printf()
+        Header files add functionality to C programs.
+
+*
+
+*
+
+*
+
+*
+
+
+*/
