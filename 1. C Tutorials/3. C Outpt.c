de644670ea703fd76d6044f892f7666cb57a3d70
Your Name <Heracleioncapital@gmail.com>
2024-08-12 21:44:25 +0300

    Outpt session complete

---
+++ 1. C Tutorials/3. C Outpt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World! \n");
+    printf("I am John \n");
+    printf("Pleased to meet yor acqintqnce \n");
+
+    return 0;
+}
+
+/*
+----------------------------------------------------------------
+Notes
+----------------------------------------------------------------
+Outpt (Print Text)
+
+    * To output value or print text in C, you can se the printf() function.
+
+Double Quotes
+
+    * When working with text, it must be wraped inside double quotes, otherwise it will not work.
+
+Many printf() functions
+
+    *you can use many printf() functions as ou want, 
+
+    NOTE: printf() function does not insert a new line at the end of the output unless (\n) is used. 
+
+*/
