4e50408c506c8e44d6dbdac03c45888084c6c741
Your Name <Heracleioncapital@gmail.com>
2024-08-14 14:55:33 +0300

    Done with C comment notes

---
+++ 1. C Tutorials/4. C Comments.c
@@ -0,0 +1,34 @@
+/* 
+----------------------------------------------------------------
+C Comments
+----------------------------------------------------------------
+
+    * Comments can be used to explain code, and to make it more readable. It can also be sed to prevent execution when testing alternative code.
+
+    * commands can be single-line or multi-line.
+
+----------------------------------------------------------------
+Single-line comments
+----------------------------------------------------------------
+    * Single-line comments start with  foward slashes (//) and is ignored b the compiler.
+
+    Example:
+
+    // This is a comment
+    printf("Hello, world");
+
+    *Comments can also be written at the end of a line of code
+
+    Example:
+
+    printf("Hello, world"); // This is a comment
+
+----------------------------------------------------------------
+Multi-line comments
+----------------------------------------------------------------
+    * Multi-line comments start with slash abd asterisk and ends with an asterisk and slash
+
+    Example:
+
+
+*/
