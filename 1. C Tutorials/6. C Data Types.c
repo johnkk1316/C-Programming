e7ae96737e39e326da5814fd0966551e717c3355
Your Name <Heracleioncapital@gmail.com>
2024-08-20 01:02:28 +0300

    Updated Data Type notes

--- 1. C Tutorials/6. C Data Types.c
+++ 1. C Tutorials/6. C Data Types.c
@@ -88,3 +88,84 @@ int main ()
         printf("myString is: %s\n\n", myString);
         return 0;
     }
+
+
+
+
+/*
+----------------------------------------------------------------------------
+The char Type
+----------------------------------------------------------------------------
+* The char data type is used to store a single character.
+
+* The character must be surrounded by single quotes, like 'A' or 'c', and we use the %c format specifier to print it:
+
+    Example
+        char myGrade = 'A';
+        printf("%c", myGrade);
+
+
+* Alternatively, if you are familiar with ASCII, you can use ASCII values to display certain characters. 
+
+----------------------------------------------------------------------------------
+| Note: These values are not surrounded by quotes (''), as they are numbers:     |
+----------------------------------------------------------------------------------
+    Example
+        char a = 65, b = 66, c = 67;
+        printf("%c", a);
+        printf("%c", b);
+        printf("%c", c);
+
+    OUTPUT
+    A
+    B
+    C
+
+
+* ASCII stands for the "American Standard Code for Information Interchange".
+
+* It was designed in the early 60's, as a standard character set for computers and electronic devices.
+
+* ASCII is a 7-bit character set containing 128 characters.
+
+* It contains the numbers from 0-9, the upper and lower case English letters from A to Z, and some special characters.
+
+* The character sets used in modern computers, in HTML, and on the Internet, are all based on ASCII.
+
+
+-----------------------------------------------------------------------------------
+Notes on Characters
+-----------------------------------------------------------------------------------
+
+* If you try to store more than a single character, it will only print the last character:
+
+    Example
+        char myText = 'Hello';
+        printf("%c", myText);
+    
+    OUTPUT
+    o
+
+------------------------------------------------------------------------------------
+Note: Don't use the char type for storing multiple characters, as it may produce   |   errors.                                                                            |
+------------------------------------------------------------------------------------
+
+
+* To store multiple characters (or whole words), use strings (which you will learn more about in a later chapter):
+
+    Example
+        char myText[] = "Hello";
+        printf("%s", myText);
+
+    OUTPUT
+    Hello    
+
+
+*   FOR NOW, JUST KNOW THAT WE ARE USING STRINGS FOR MULTIPLE CHARACTERS/TEXT, AND THE 
+    CHAR TYPE FOR SINGLE CHARACTERS.
+
+
+
+
+    
+*/
