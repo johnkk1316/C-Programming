63dee4067aeca3982974d5425de99370f57c335d
Your Name <Heracleioncapital@gmail.com>
2024-08-19 21:23:42 +0300

    fixed bugs in assignments

--- 29. Assignments/rectangleVolume.c
+++ 29. Assignments/rectangleVolume.c
@@ -11,21 +11,20 @@ Lenth = 15
 Width = 10
 Height = 3
 
-Volume Formula = L * W * H 
+Volume Formula = L * W * H
 
-Squared shortut = Alt + 0178 = squared²  
+Squared shortut = Alt + 0178 = squared²
 Cubed shortcut  = Alt + 0179 = cubed³
 
 */
 
-#include<stdio.h>
-
-main(void)
-{
-    int lenth = 15, width = 10, height = 3;
-    int volume = lenth * width * height;
-
-    printf("\nThe Volume is:\n%d\n", volume);
-
-    return 0;
-}
+#include <stdio.h>
+int main()
+    {
+        int length = 15, width = 10, height = 3;
+        int volume = length * width * height;
+    
+        printf("\nThe Volume is:\n%d\n", volume);
+    
+        return 0;
+    }
