33cc48dc97a291fc779eec4e483f300e0ceb83db
Your Name <Heracleioncapital@gmail.com>
2024-08-22 01:17:47 +0300

    renamed assignment files

---
+++ 29. Assignments/2. rectangle_volume.c
@@ -0,0 +1,30 @@
+// VOLUME OF A RECTANGLE
+// AUTHOR K.J
+
+/*
+
+-------------------------------------------------------
+TASK: CALCULATE VOLUME OF A RECTANGLE:
+-------------------------------------------------------
+L²  ³
+Lenth = 15
+Width = 10
+Height = 3
+
+Volume Formula = L * W * H
+
+Squared shortut = Alt + 0178 = squared²
+Cubed shortcut  = Alt + 0179 = cubed³
+
+*/
+
+#include <stdio.h>
+int main()
+    {
+        int length = 15, width = 10, height = 3;
+        int volume = length * width * height;
+    
+        printf("\nThe Volume is:\n%d\n", volume);
+    
+        return 0;
+    }
