346b274500d8151a6c490476cefa5bd2d4c14a73
Your Name <Heracleioncapital@gmail.com>
2024-08-27 14:22:04 +0300

    constants

---
+++ Worksheet/3.Constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+    {
+        //Constant or const = fixed value that can not be altered b the program during its execution
+
+        const double PI = 3.14159;
+        printf("\n%lf\n", PI); 
+
+        return 0;
+    }
