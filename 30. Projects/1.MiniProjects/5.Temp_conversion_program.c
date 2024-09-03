02ac716c4f90e538739510207dd654f8c6e23bbb
johnkk1316 <codeona4@gmail.com>
2024-09-03 15:50:36 +0300

    Switch statement on temp conversion program

--- 30. Projects/1.MiniProjects/5.Temp_conversion_program.c
+++ 30. Projects/1.MiniProjects/5.Temp_conversion_program.c
@@ -1,37 +1,83 @@
 #include <stdio.h>
 #include <ctype.h>  //The <ctype.h> header in C provides functions for character classification and conversion, such as toupper, tolower, isalpha, isdigit, and others.
 
+// IF STATEMENT
+
+//int main()
+//    {
+//        char unit;
+//        double temp;
+//
+//        // input if temp is (C) OR  (F).
+//        printf("Is temp (F) or (C)?: ");
+//        scanf("%c", &unit);
+//
+//        unit = toupper(unit); // The toupper function in C converts a lowercase letter to its uppercase equivalent, returning the character unchanged if it is not a lowercase letter. THis is at all possible because of the header file <ctype.h>
+//
+//        if(unit == 'C' ){
+//            printf("Input temp in Celcius: ");
+//            scanf("%lf", &temp);
+//
+//            temp = (temp * 9/5) + 32;
+//            printf("The temp is: %.2lf (F)", temp);
+//        }
+//
+//        else if (unit == 'F'){
+//            printf("Input temp in (F): ");
+//            scanf("%lf", &temp);
+//
+//            temp = (temp - 32) * 5 / 9;
+//            printf("The temp is: %.2lf (C)", temp);
+//        }
+//
+//
+//        else
+//        {
+//            printf("%c is not a valid unit of measrement",unit);
+//        }
+//
+//    }
+
+
+// SWITCH STATEMENT
+
 int main()
     {
         char unit;
         double temp;
 
-        // input if temp is (C) OR  (F).
-        printf("Is temp (F) or (C)?: ");
+        // INPUT UNIT
+
+
+        printf("Choose unit (C) OR (F): ");
         scanf("%c", &unit);
 
-        unit = toupper(unit); // The toupper function in C converts a lowercase letter to its uppercase equivalent, returning the character unchanged if it is not a lowercase letter. THis is at all possible because of the header file <ctype.h>
+        unit = toupper(unit);
+        // IF UNIT IS (C).
+        switch(unit){
+            case 'C':
+                printf("Input temp in (C): ");
+                scanf("%lf", &temp);
 
-        if(unit == 'C' ){
-            printf("Input temp in Celcius: ");
-            scanf("%lf", &temp);
+                temp = (temp * 5 / 9) + 32;
+                printf("Temp in (F): %.2lf\n", temp);
+            break;
 
-            temp = (temp * 9/5) + 32;
-            printf("The temp is: %.2lf (F)", temp);
+            // IF UNIT IS (F)
+            case 'F':
+                printf("Input temp in (F): ");
+                scanf("%lf", &temp);
+
+                temp = (temp - 32) * 5 / 9;
+                printf("Temp in (C): %.2lf\n", temp);
+            break;
+
+            default:
+                printf("%c is invalid!!\n", unit);
         }
 
-        else if (unit == 'F'){
-            printf("Input temp in (F): ");
-            scanf("%lf", &temp);
-
-            temp = (temp - 32) * 5 / 9;
-            printf("The temp is: %.2lf (C)", temp);
-        }
 
 
-        else
-        {
-            printf("%c is not a valid init of measrement",unit);
-        }
 
+        return 0;
     }
