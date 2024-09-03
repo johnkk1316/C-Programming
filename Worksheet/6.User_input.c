60530c2e72d9baeca9ac1ffe2bdd240b1b984d3d
johnkk1316 <codeona4@gmail.com>
2024-09-03 21:23:50 +0300

    Modified

--- Worksheet/6.User_input.c
+++ Worksheet/6.User_input.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h> // a C header file that provides functions for working with strings, like measuring length and copying.
 
-int main() 
+int main()
     {
         char name[25]; //[25] is bytes limit
         int age;
 
         printf("What's your name:");
+
         // scanf("%s", &name);     // scanf reads input until the first whitespace.
 
         fgets(name, 25, stdin); //  reads an entire line, including spaces, up to a specified limit.
@@ -27,4 +28,4 @@ int main()
 
     // fgets (name,25, stdin); - reads an entire line, including spaces, up to a specified limit.
 
-    // name[strlen(name)-1] = '\0';    = This line of code removes the trailing newline character (\n) from a string, typically after using fgets to read input. It does this by replacing the last character of the string (the newline) with a null terminator ('\0'), effectively ending the string before the newline.Always include header file=  <string.h> 
+    // name[strlen(name)-1] = '\0';    = This line of code removes the trailing newline character (\n) from a string, typically after using fgets to read input. It does this by replacing the last character of the string (the newline) with a null terminator ('\0'), effectively ending the string before the newline.Always include header file=  <string.h>
